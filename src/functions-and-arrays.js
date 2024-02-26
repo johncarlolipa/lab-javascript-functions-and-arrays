c936ee537cb13fd4b2b222d00773a8ef624055e6
John Carlo Lipa <johncarlolipa@gmail.com>
2024-02-26 22:11:06 +0800

    iteration 4 done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -46,7 +46,17 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+
+function averageNumbers(numbersAvg) {
+  let average = 0
+  let totalSum = 0
+  for(let i = 0; i < numbersAvg.length; i++){
+    totalSum += numbersAvg[i]
+  }
+  return average = totalSum / numbersAvg.length
+}
+
+console.log(averageNumbers(numbersAvg))
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
